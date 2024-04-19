04ef85906f3930dcbf509432ebaf855bee82e97a
rccdoes <itschauhan101@gmail.com>
2024-04-20 01:32:46 +0530

    bar 3D

--- Home/src/bar3d.c
+++ Home/src/bar3d.c
@@ -9,10 +9,14 @@ int main()
     initgraph(&gd, &gm, data);
 
     outtextxy(50, 30, "Reactnagle :");
-    rectangle(50, 50, 100, 100);
+    rectangle(50, 50, 100, 200);
 
     outtextxy(50, 130, "Bar : ");
     bar(50, 150, 200, 200);
+
+    outtextxy(50, 300, "Bar3D : ");
+    bar3d(50, 350, 200, 200, 20, 1);
+
     getch();
     closegraph();
     return (0);
