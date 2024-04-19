149bae0c67366d70aed1ea205e91376cc051d599
rccdoes <itschauhan101@gmail.com>
2024-04-19 17:03:11 +0530

    outtext

--- Home/src/shapes.cpp
+++ Home/src/shapes.cpp
@@ -6,23 +6,24 @@ int main()
 
     char data[] = "C:\\MinGW\\lib\\libbgi.a"; //static file
 
-    initgraph(&gd,&gm, data); 
-    
-    setbkcolor(GREEN);  
+    initgraph(&gd,&gm, data);
 
-    printf("\t\t\t\n\nLINE");  
-    line(50,40,190,40);  
+    setbkcolor(GREEN);
 
-    printf("\t\t\n\n\n\nRECTANGLE");  
-    rectangle(125,115,215,165); 
+    printf("\t\t\t\n\n LINE");
+    line(50, 40, 190, 40);
 
-    printf("\t\t\t\n\n\n\n\n\n\nARC");  
-    arc(120,200,180,0,30);  
+    printf("\t\t\n\n\n\n RECTANGLE");
+    rectangle(125, 115, 215, 165);
 
-    printf("\t\n\n\n\nCIRCLE");  
-    circle(120,270,30);  
+    printf("\t\t\t\n\n\n\n\n\n\n ARC");
+    arc(120, 200, 180, 0, 30);
 
-    printf("\t\n\n\n\nECLIPSE");  
+    printf("\t\n\n\n\n CIRCLE");
+    circle(120, 270, 30);
+
+    printf("\t\n\n\n\n ECLIPSE");
+    outtext("\t\n\n\n\n ECLIPSE");
     ellipse(120,350,0,360,30,20);  
 
 
