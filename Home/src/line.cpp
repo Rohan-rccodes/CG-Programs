be074a717d329233c359f0f3bde4f71caee0e395
rccdoes <itschauhan101@gmail.com>
2024-04-19 17:13:58 +0530

    line

---
+++ Home/src/line.cpp
@@ -0,0 +1,16 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main()
+{
+    int gd = DETECT, gm;
+
+    initgraph(&gd, &gm, "C:\TurboC++\Disk\TurboC3\BIN");
+
+    line(50, 50, 100, 50);
+
+    getch();
+    closegraph();
+
+    return (0);
+}
