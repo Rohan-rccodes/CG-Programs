d667130fa4c37bc9d0a78ac7f2bfabeff131f9d7
rccdoes <itschauhan101@gmail.com>
2024-04-20 02:55:38 +0530

    all shapes

---
+++ Home/src/all-shapes.c
@@ -0,0 +1,21 @@
+#include <graphics.h>
+#include <conio.h>
+
+int main()
+{
+    int gd = DETECT, gm;
+    char data[] = "c:\MinGW\bin";
+    initgraph(&gd, &gm, data);
+
+    outtextxy(30, 30, "All THE SHAPES:");
+
+    rectangle(30, 60, 100, 100);
+
+    circle(60, 150, 30);
+
+    line(30, 200, 200, 200);
+
+    getch();
+    closegraph();
+    return (0);
+}
